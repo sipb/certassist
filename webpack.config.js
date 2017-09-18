0ad5b026208151c343bd6408f4aa7df895724bb8
Anders Kaseorg <andersk@mit.edu>
2017-09-17 20:20:37 -0400

    Use html-webpack-plugin for hashed .js/.css URLs
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
     entry: './certassist.js',
     output: {
-        filename: 'web/certassist.bundle.js'
+        filename: '[name].[chunkhash].bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.ejs'
+        })
+    ],
     module: {
-        loaders: [{
+        rules: [{
             test: /\.js$/,
             exclude: /node_modules/,
             loader: 'babel-loader'
+        }, {
+            test: /\.css$/,
+            use: [
+                'file-loader?name=[name].[hash].[ext]',
+                'extract-loader',
+                'css-loader'
+            ]
         }]
     }
 };
