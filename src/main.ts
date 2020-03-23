a311397131cde4a654f8315be52409de79dbfbb2
Anders Kaseorg <andersk@mit.edu>
2020-03-23 00:52:14 -0700

    Polyfill ParentNode.append for IE
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "js-polyfills/dom.js";
+
 import "./certassist.css"; // eslint-disable-line import/no-unassigned-import
 import "./mit"; // eslint-disable-line import/no-unassigned-import
 import "./csail"; // eslint-disable-line import/no-unassigned-import
