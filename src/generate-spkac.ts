3629530c73e7715dd3fee8f43ca1c0996cca4740
Anders Kaseorg <andersk@mit.edu>
2021-07-27 15:48:23 -0700

    Upgrade SPKAC digest from MD5 to SHA-256
    This is now allowed by
    https://datatracker.ietf.org/doc/html/draft-leggett-spkac-00.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- src/generate-spkac.ts
+++ src/generate-spkac.ts
@@ -8,7 +8,7 @@ function generateSpkac(
     forge.pki.publicKeyToAsn1(keyPair.publicKey),
     asn1.create(asn1.Class.UNIVERSAL, asn1.Type.IA5STRING, false, challenge),
   ]);
-  const md = forge.md.md5.create();
+  const md = forge.md.sha256.create();
   md.update(asn1.toDer(pkac).getBytes());
   const signature = keyPair.privateKey.sign(md);
   return forge.util.encode64(
@@ -21,7 +21,7 @@ function generateSpkac(
               asn1.Class.UNIVERSAL,
               asn1.Type.OID,
               false,
-              asn1.oidToDer(forge.pki.oids.md5WithRSAEncryption).getBytes()
+              asn1.oidToDer(forge.pki.oids.sha256WithRSAEncryption).getBytes()
             ),
             asn1.create(asn1.Class.UNIVERSAL, asn1.Type.NULL, false, ""),
           ]),
