e8e97d3fb5a45bbbb1f585596ba8385b234d33d1
Anders Kaseorg <andersk@mit.edu>
2024-06-29 01:16:58 -0400

    Set Referer header
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- src/mit.ts
+++ src/mit.ts
@@ -183,6 +183,7 @@ async function downloadCertServerKey(options: Options): Promise<void> {
 const caHeaders = {
   Connection: "close",
   Host: "ca.mit.edu",
+  Referer: "https://ca.mit.edu/ca/",
   "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0",
 };
